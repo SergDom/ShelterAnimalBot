bc7391777cc125eb1b55cd2834196f18b18b00a5
sergeydomanov <nomad-labs@yandex.ru>
2023-03-23 20:16:03 +0300

    Master branch

---
+++ src/main/java/com/javadreamteam/shelteranimalbot/ShelterAnimalBotApplication.java
@@ -0,0 +1,13 @@
+package com.javadreamteam.shelteranimalbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterAnimalBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterAnimalBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javadreamteam/shelteranimalbot/ShelterAnimalBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadreamteam.shelteranimalbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterAnimalBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
