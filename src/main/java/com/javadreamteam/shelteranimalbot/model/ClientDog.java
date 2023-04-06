e6f4875d6b7a1088d1f38932fd27c5f7b1c2573e
sergeydomanov <nomad-labs@yandex.ru>
2023-04-06 20:30:20 +0300

    Добавил константы для кнопок. Добавил опсиание сервисов и модель Status с костантами статуса владельца.

--- src/main/java/com/javadreamteam/shelteranimalbot/model/ClientDog.java
+++ src/main/java/com/javadreamteam/shelteranimalbot/model/ClientDog.java
@@ -28,7 +28,7 @@ public class ClientDog {
     private Integer yearOfBirth;
 
     @Column(name = "status")
-    private String status;
+    private Status status;
 
     @OneToOne
     @JoinColumn(name = "dog_id")
@@ -85,11 +85,11 @@ public class ClientDog {
         this.yearOfBirth = yearOfBirth;
     }
 
-    public String getStatus() {
+    public Status getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(Status status) {
         this.status = status;
     }
 
--- src/main/java/com/javadreamteam/shelteranimalbot/service/ClientDogService.java
+++ src/main/java/com/javadreamteam/shelteranimalbot/service/ClientDogService.java
@@ -9,6 +9,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
+/**
+ * Сервис ClientDogService
+ * Сервис используется для создания, редактирования, удаления и получения владельца собаки/списка владельцев собак из БД
+ */
+
 @Service
 public class ClientDogService {
 
@@ -20,16 +25,40 @@ public class ClientDogService {
         this.repository = repository;
     }
 
+    /**
+     * Создание владельца собаки и сохранение его в БД
+     * <br>
+     * Используется метод репозитория {@link ClientDogRepository#save(Object)}
+     * @param clientDog создается объект владелец собаки
+     * @return созданный владелец
+     */
+
     public ClientDog create(ClientDog clientDog) {
         logger.info("Was invoked method to create a personDog");
         return repository.save(clientDog);
     }
 
+    /**
+     * Поиск владельца собаки в БД по id
+     * <br>
+     * Используется метод репозитория {@link ClientDogRepository#findById(Object)}
+     * @param id идентификатор владельца
+     * @throws ClientException, если владелец с указанным id не найден
+     * @return найденный владелец
+     */
     public ClientDog getById(Long id) {
         logger.info("Was invoked method to get a Client by id={}", id);
         return repository.findById(id).orElseThrow(ClientException::new);
     }
 
+    /**
+     * Изменение данных владельца собаки в БД
+     * <br>
+     * Используется метод репозитория {@link ClientDogRepository#save(Object)}
+     * @param clientDog изменяемый владелец
+     * @throws ClientException, если указанный владелец собаки не найден
+     * @return измененный владелец собаки
+     */
     public ClientDog update(ClientDog clientDog) {
         logger.info("Was invoked method to update a personDog");
         if (clientDog.getId() != null) {
@@ -39,16 +68,33 @@ public class ClientDogService {
         }
         throw new ClientException();
     }
-
+    /**
+     * Удаление владельца собаки из БД по id
+     * <br>
+     * Используется метод репозитория {@link ClientDogRepository#delete(Object)}
+     * @param id идентификатор владельца собаки
+     */
     public void removeById(Long id) {
         logger.info("Was invoked method to remove a Client by id={}", id);
         repository.deleteById(id);
     }
+    /**
+     * Получение списка владельцев собак из БД
+     * <br>
+     * Используется метод репозитория {@link ClientDogRepository#findAll()}
+     * @return список владельцев собак
+     */
 
     public Collection<ClientDog> getAll() {
         logger.info("Was invoked method to get all Clients with dogs");
         return repository.findAll();
     }
+    /**
+     * Получение списка владельцев собак из БД по id
+     * <br>
+     * Используется метод репозитория {@link ClientDogRepository Set<ClientDog>#findByChatId()}
+     * @return список владельцев собак
+     */
 
     public Collection<ClientDog> getByChatId(Long chatId) {
         logger.info("Was invoked method to get a Client with dog by chatId={}", chatId);
