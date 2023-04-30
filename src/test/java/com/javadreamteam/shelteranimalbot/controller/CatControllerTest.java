f4b658907f6a54faea476c861880b06d1a676ac1
Alexust11 <110272504+Alexust11@users.noreply.github.com>
2023-04-30 22:32:04 +0700

    некторые исправления

--- src/test/java/com/javadreamteam/shelteranimalbot/controller/CatControllerTest.java
+++ src/test/java/com/javadreamteam/shelteranimalbot/controller/CatControllerTest.java
@@ -25,96 +25,96 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest (CatController.class)
 public class CatControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @MockBean
-    private CatRepository catRepository;
-
-
-
-    @MockBean
-    private CatService catService;
+	@MockBean
+	private CatRepository catRepository;
 
 
 
-    @InjectMocks
-    private CatController catController;
+	@MockBean
+	private CatService catService;
 
 
-    private final Cat animal = new Cat();
-    private final JSONObject jsonAnimal = new JSONObject();
 
-    @BeforeEach
-    public void setup() throws Exception {
+	@InjectMocks
+	private CatController catController;
 
-        animal.setId(1L);
-        animal.setName("Мурка");
-        animal.setAge(2);
-        animal.setBreed("Британская");
-        animal.setInfo("В крапинку");
 
-        jsonAnimal.put("id", animal.getId());
-        jsonAnimal.put("name", animal.getName());
-        jsonAnimal.put("age", animal.getAge());
-        jsonAnimal.put("breed", animal.getBreed());
-        jsonAnimal.put("info", animal.getInfo());
+	private final Cat animal = new Cat();
+	private final JSONObject jsonAnimal = new JSONObject();
 
-        when(catService.create(animal)).thenReturn(animal);
-        when(catService.update(animal)).thenReturn(animal);
-        when(catService.getById(any())).thenReturn(animal);
-        when(catService.getAll()).thenReturn(List.of(animal));
-    }
+	@BeforeEach
+	public void setup() throws Exception {
 
-    @Test
-    public void createCat () throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/cats")
-                        .content(jsonAnimal.toString())
-                        .contentType(MediaType.APPLICATION_JSON))
+		animal.setId(1L);
+		animal.setName("Мурка");
+		animal.setAge(2);
+		animal.setBreed("Британская");
+		animal.setInfo("В крапинку");
 
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value(animal.getName()))
-                .andExpect(jsonPath("$.age").value(animal.getAge()))
-                .andExpect(jsonPath("$.breed").value(animal.getBreed()))
-                .andExpect(jsonPath("$.info").value(animal.getInfo()));
-    }
+		jsonAnimal.put("id", animal.getId());
+		jsonAnimal.put("name", animal.getName());
+		jsonAnimal.put("age", animal.getAge());
+		jsonAnimal.put("breed", animal.getBreed());
+		jsonAnimal.put("info", animal.getInfo());
 
-    @Test
-    public void updateCat () throws Exception {
-        animal.setName("New name");
-        mockMvc.perform(MockMvcRequestBuilders
-                        .put("/cats")
-                        .content(jsonAnimal.toString())
-                        .contentType(MediaType.APPLICATION_JSON))
+		when(catService.create(animal)).thenReturn(animal);
+		when(catService.update(animal)).thenReturn(animal);
+		when(catService.getById(any())).thenReturn(animal);
+		when(catService.getAll()).thenReturn(List.of(animal));
+	}
 
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value(animal.getName()))
-                .andExpect(jsonPath("$.age").value(animal.getAge()))
-                .andExpect(jsonPath("$.breed").value(animal.getBreed()))
-                .andExpect(jsonPath("$.info").value(animal.getInfo()));
-    }
+	@Test
+	public void createCat () throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders
+						.post("/cats")
+						.content(jsonAnimal.toString())
+						.contentType(MediaType.APPLICATION_JSON))
 
-    @Test
-    public void removeCat () throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .delete("/cats/1")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value(animal.getName()))
+				.andExpect(jsonPath("$.age").value(animal.getAge()))
+				.andExpect(jsonPath("$.breed").value(animal.getBreed()))
+				.andExpect(jsonPath("$.info").value(animal.getInfo()));
+	}
 
-    @Test
-    public void getCat () throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/cats/1")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-    @Test
-    public void getCatAll () throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/cats/find-all-cats")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
+	@Test
+	public void updateCat () throws Exception {
+		animal.setName("New name");
+		mockMvc.perform(MockMvcRequestBuilders
+						.put("/cats")
+						.content(jsonAnimal.toString())
+						.contentType(MediaType.APPLICATION_JSON))
+
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value(animal.getName()))
+				.andExpect(jsonPath("$.age").value(animal.getAge()))
+				.andExpect(jsonPath("$.breed").value(animal.getBreed()))
+				.andExpect(jsonPath("$.info").value(animal.getInfo()));
+	}
+
+	@Test
+	public void removeCat () throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders
+						.delete("/cats/1")
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void getCat () throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders
+						.get("/cats/1")
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+	@Test
+	public void getCatAll () throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders
+						.get("/cats/find-all-cats")
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
 }
--- src/test/java/com/javadreamteam/shelteranimalbot/service/ClientDogServiceTest.java
+++ src/test/java/com/javadreamteam/shelteranimalbot/service/ClientDogServiceTest.java
@@ -9,15 +9,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-
+import static org.mockito.Mockito.*;
 class ClientDogServiceTest {
     @Mock
     private ClientDogRepository repository;
-    @InjectMocks
     private ClientDogService clientDogService;
 
 
@@ -29,15 +29,23 @@ class ClientDogServiceTest {
 
     @Test
     void testCreate() {
-        ClientDog clientDog = new ClientDog("John", 123456789L, "123-456-7890");
-         assertNull(clientDog.getId());
+        ClientDog clientDog = new ClientDog();
+        clientDog.setName("John");
+        clientDog.setChatId(123456L);
 
-        clientDogService.create(clientDog);
+        when(repository.save(clientDog)).thenReturn(clientDog);
 
-        assertNotNull(clientDog.getId());
+        ClientDog createdClientDog = clientDogService.create(clientDog);
+
+        assertNotNull(createdClientDog);
+        assertEquals(clientDog.getName(), createdClientDog.getName());
+        assertEquals(clientDog.getChatId(), createdClientDog.getChatId());
+
+        verify(repository, times(1)).save(clientDog);
     }
 
 
+//
     @Test
     public void testGetById() {
         Long id = 1L;
@@ -52,40 +60,71 @@ class ClientDogServiceTest {
         ClientDog result = clientDogService.getById(id);
 
         assertEquals(clientDog, result);
-    }
-    @Test
-    public void testUpdateSuccess() {
-        Long id = 1L;
-        ClientDog clientDog = new ClientDog();
-        clientDog.setId(id);
-        clientDog.setName("Test");
-        clientDog.setChatId(123456789L);
-        clientDog.setPhoneNumber("1234567890");
 
-        when(repository.getById(id)).thenReturn(clientDog);
-        when(repository.save(clientDog)).thenReturn(clientDog);
-
-        ClientDog result = clientDogService.update(clientDog);
-
-        assertEquals(clientDog, result);
-    }
-
-    @Test
-    public void testUpdateFail() {
-        Long id = 1L;
-        ClientDog clientDog = new ClientDog();
-        clientDog.setId(id);
-        clientDog.setName("Test");
-        clientDog.setChatId(123456789L);
-        clientDog.setPhoneNumber("1234567890");
-
-        when(repository.getById(id)).thenReturn(null);
-
-        clientDogService.update(clientDog);
-    }
+ }
 
 
+@Test
+public void testRemoveById() {
 
+    ClientDog clientDog = new ClientDog();
+    clientDog.setId(1L);
+    clientDog.setName("Test Dog");
+    repository.save(clientDog);
+    clientDogService.removeById(1L);
+    assertFalse(repository.existsById(1L));
 }
+    @Test
+    public void testGetAll() {
+        // Создаем тестовые сущности в базе данных
+        ClientDog clientDog1 = new ClientDog();
+        clientDog1.setId(1L);
+        clientDog1.setName("Test Dog 1");
+        repository.save(clientDog1);
+
+        ClientDog clientDog2 = new ClientDog();
+        clientDog2.setId(2L);
+        clientDog2.setName("Test Dog 2");
+        repository.save(clientDog2);
+
+        // Вызываем метод getAll()
+        Collection<ClientDog> allClientDogs = clientDogService.getAll();
+
+        // Проверяем, что количество полученных сущностей соответствует количеству созданных в базе данных
+        assertEquals(2, allClientDogs.size());
+
+        // Проверяем, что полученные сущности содержат правильные данные
+        assertTrue(allClientDogs.stream().anyMatch(cd -> cd.getId().equals(1L) && cd.getName().equals("Test Dog 1")));
+        assertTrue(allClientDogs.stream().anyMatch(cd -> cd.getId().equals(2L) && cd.getName().equals("Test Dog 2")));
+    }
+
+    @Test
+    void getByChatId() {
+        // Создаем тестовую сущность в базе данных
+        ClientDog clientDog = new ClientDog();
+        clientDog.setId(1L);
+        clientDog.setName("Test Dog");
+        clientDog.setChatId(123456789L);
+        repository.save(clientDog);
+
+        // Вызываем метод getByChatId() с существующим chatId
+        ClientDog foundClientDog = clientDogService.getByChatId(123456789L);
+
+        // Проверяем, что найденная сущность не null и содержит правильные данные
+        assertNotNull(foundClientDog);
+        assertEquals(1L, foundClientDog.getId().longValue());
+        assertEquals("Test Dog", foundClientDog.getName());
+        assertEquals(123456789L, foundClientDog.getChatId().longValue());
+
+        // Вызываем метод getByChatId() с несуществующим chatId
+        ClientDog notFoundClientDog = clientDogService.getByChatId(987654321L);
+
+        // Проверяем, что не найдена никакая сущность
+        assertNull(notFoundClientDog);
+
+    }
+}
+
+
 
 
